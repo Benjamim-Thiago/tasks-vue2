54d43fab85d252b5719cb9cca9717cb96daa4e4e
Benjamim Thiago <bebenjamimthiago@gmail.com>
2018-09-25 17:44:44 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" class="container">
+    <list-tasks-component></list-tasks-component>
+  </div>
+</template>
+
+<script>
+import ListTasksComponent from './components/tasks/ListTasksComponent'
+export default {
+  name: 'app',
+  components: {
+    ListTasksComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/tasks/ListTasksComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+            <table class="table table-striped">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Nome</th>
+                        <th width="180px">Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item, index) in tasks" :key="index">
+                        <td>{{item.id}}</td>
+                        <td>{{item.name}}</td>
+                        <td>
+                            <a href="">Editar</a> | 
+                            <a href="">Deletar</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'Lista de Tarefas',
+            tasks:[]
+        }
+    }
+}
+</script>
+
+<style scoped>
+   
+</style>
